b9a58948c3b1d109213ed83f39b5dd6d40a0fcf6
Jason L Perry <jasper@ambethia.com>
2016-11-27 15:05:18 -0500

    Hello, functions

---
+++ README.md
@@ -0,0 +1,39 @@
+# Function Junction
+
+## Objectives
+
+After completing this assignment, you should be able to:
+
+* Demonstrate understanding of types, functions, argument-handling, loops, and conditional statements
+
+## Details
+
+Instructions
+
+1. Fork [this repository](https://gibhub.com/tiy-tpa-fee/function-junction) to your own account.
+2. Change into today's directory: `take ~/tiy/week-3/day-1`
+3. Clone your repository: `hub clone function-junction`
+4. Change into your project's directory: `cd function-junction`
+5. Install the dependencies: `yarn` or `npm install`
+6. Open in Atom: `atom .`
+7. Start test runner: `yarn test` or `npm test`
+7. Open `src/functions.test.js` and work on functions until tests pass.
+8. Commit and push your work to github.
+
+## Normal Mode
+
+- All tests passing
+- No linter warnings
+- Turn in the homework using the URL to your repository on Github.
+
+## Adventure Mode
+
+- If you used "built-in" JavaScript methods like `String.reverse()` or `Array.max()` to solve these problems, try implementing them from scratch.
+- Vice-versa: if you implemented your solutions from scratch, look in the documentation linked below to see if JavaScript has any built-in functions to help solve these problems.
+
+## Additional Resources
+
+Reference the documentation on MDN to find what kind of helpful functions might already be on `Array` and `String` in JavaScript.
+
+- [String Functions on MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String).
+- [Array Functions on MDN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array).
---
+++ src/functions.test.js
@@ -0,0 +1,130 @@
+/**
+ * Define a function max() that takes two numbers as arguments
+ * and returns the largest of them. Use the if-then-else
+ * construct available in Javascript.
+ */
+
+// ...
+
+/**
+ * Define a function maxOfThree() that takes three
+ * numbers as arguments and returns the largest of them.
+ */
+
+// ...
+
+/*
+ * Define a function sum() that takes two numbers as
+ * arguments and computes the sum of those two numbers.
+ */
+
+// ...
+
+/*
+ * Define a function sumOfArray that calculates the sum of
+ * all the numbers in an array.
+ */
+
+// ...
+
+/**
+ * Write a function isVowel() that takes a character (i.e. a string of length 1)
+ * and returns true if it is a vowel, false otherwise.
+ */
+
+// ...
+
+ /**
+  * Write a function rovarspraket() that will translate
+  * a text into a "rövarspråket". That is, double every
+  * consonant and place an occurrence of "o" in between.
+  *
+  * For example, rovarspraket("this is fun") should
+  * return the string "tothohisos isos fofunon".
+  */
+
+// ...
+
+/**
+ * Define a function reverse() that computes
+ * the reversal of a string. For example,
+ * reverse("skoob") should return the
+ * string "books".
+ */
+
+// ...
+
+ /**
+  * Write a function findLongestWord() that takes an
+  * string returns the first, longest word in the array.
+  *
+  * i.e. findLongestWord("book dogs") should return "book"
+  */
+
+// ...
+
+/**
+ * NOTE: Don't modify anything below this line...
+ */
+
+/* eslint-disable no-undef */
+
+import test from 'ava'
+
+test('max()', (t) => {
+  t.is(max(1, 3), 3)
+  t.is(max(0, 3), 3)
+  t.is(max(10, 3), 10)
+  t.is(max(-1, -3), -1)
+  t.is(max('aaa', 0), 0)
+  t.true(isNaN(max('aaa', 'bbb')))
+})
+
+test('maxOfThree()', (t) => {
+  t.is(maxOfThree(1, 3, 2), 3)
+  t.is(maxOfThree(0, 3, -1), 3)
+  t.is(maxOfThree(10, 3, 50), 50)
+  t.is(maxOfThree(-1, -3, -10), -1)
+  t.is(maxOfThree('aaa', 0, 1), 1)
+  t.true(isNaN(maxOfThree('aaa', 'bbb', 'ccc')))
+})
+
+test('sum()', (t) => {
+  t.is(sum(8, 11), 19)
+  t.is(sum(4, 100), 104)
+})
+
+test('sumOfArray()', (t) => {
+  t.is(sumOfArray([1, 2]), 3)
+  t.is(sumOfArray([1, 2, 3]), 6)
+  t.is(sumOfArray([10, 9, 8]), 27)
+  t.is(sumOfArray([]), 0)
+})
+
+test('isVowel()', (t) => {
+  t.is(isVowel(0), false)
+  t.is(isVowel('B'), false)
+  t.is(isVowel('b'), false)
+  t.is(isVowel('a'), true)
+  t.is(isVowel('E'), true)
+})
+
+test('rovarspraket()', (t) => {
+  t.is(rovarspraket('a'), 'a')
+  t.is(rovarspraket('b'), 'bob')
+  t.is(rovarspraket('cat'), 'cocatot')
+  t.is(rovarspraket('javascript'), 'jojavovasoscocroripoptot')
+  t.is(rovarspraket(0), '0')
+})
+
+test('reverse()', (t) => {
+  t.is(reverse('books'), 'skoob')
+  t.is(reverse("we don't want no trouble"), "elbuort on tnaw t'nod ew")
+})
+
+test('findLongestWord()', (t) => {
+  t.is(findLongestWord('book dogs'), 'book')
+  t.is(findLongestWord('everything'), 'life the universe and everything')
+})
+
+/* eslint-enable */
